8c73e4713476d1642b7e4ec77d4657e370827e8d
xiaodi.ding <xiaodi.ding@weimob.com>
2021-05-06 20:35:00 +0800

    leetcode test

---
+++ src/main/java/leetcode/editor/cn/test2.java
@@ -0,0 +1,89 @@
+//给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。 
+//
+// 示例 1: 
+//
+// 输入: 123
+//输出: 321
+// 
+//
+// 示例 2: 
+//
+// 输入: -123
+//输出: -321
+// 
+//
+// 示例 3: 
+//
+// 输入: 120
+//输出: 21
+// 
+//
+// 注意: 
+//
+// 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231, 231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。 
+// Related Topics 数学 
+// 👍 2308 👎 0
+
+package leetcode.editor.cn;
+
+public class test2 {
+    public static void main(String[] args) {
+        Solution solution = new test2().new Solution();
+     }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+       /* public int test(int x) {
+
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //TODO 反转整型
+        public int reverse(int x) {
+            long y = 0;
+            while (x != 0) {
+                y = y * 10 + x % 10;
+                x = x / 10;
+            }
+            if (y > Integer.MAX_VALUE || y < Integer.MIN_VALUE)
+                return 0;
+            return (int) y;
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
